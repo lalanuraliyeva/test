47ac2747b4c5be8a82e0ab1281706d619a3adc35
Lala Nuraliyeva <nuralievalala@gmail.com>
2024-12-15 17:08:03 +0100

    exception handling

---
+++ src/Exception.java
@@ -0,0 +1,21 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Exception {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your age: ");
+        String name = scanner.nextLine();
+
+        try{
+            if (name.equals("Thomas")) {
+                System.out.println("You are not allowed!");
+            } else {
+                System.out.println("Welcome! " + name);
+            }
+        } catch (InputMismatchException e){
+            e.printStackTrace();
+        }
+    }
+}
